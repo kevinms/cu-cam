e572231ac3893494d4f56a6fc50c6de53dcc8a21
kevin4@g.clemson.edu <kevin4@g.clemson.edu@177bddce-22ae-11df-9cb1-5138f70e336d>
2010-03-01 14:40:48 +0000

    Updated timeline, some small tests for auth system (which is on pause now)

---
+++ trunk/src/auth-test.c
@@ -0,0 +1,6 @@
+#include "auth.h"
+
+int main()
+{
+	return 0;
+}
---
+++ trunk/src/auth.h
@@ -0,0 +1,9 @@
+#ifndef AUTH_H_
+#define AUTH_H_
+
+#include <gnutls/gnutls.h>
+#include <gnutls/extra.h>  /* LZO Compression */
+
+void net_connect_TLS();
+
+#endif /* !AUTH_H_ */
