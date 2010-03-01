2213909f07ea2e1621696abfc23b49b957447c1e
kevin4@g.clemson.edu <kevin4@g.clemson.edu@177bddce-22ae-11df-9cb1-5138f70e336d>
2010-03-01 15:38:52 +0000

    renamed cam-admin.c to cam-client.c and added a header

---
+++ trunk/src/cam-client.c
@@ -0,0 +1,8 @@
+/* 
+ * Client program
+ * 
+ * Allow admin and students to send commands to all servers.  Depending
+ * on what permissions the user has will determine what commands they 
+ * can perform.
+ */
+
