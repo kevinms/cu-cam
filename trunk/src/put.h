54f4cc29df1bae3d8e6482336cbbbc6ea84e7046
Drago3711 <Drago3711@177bddce-22ae-11df-9cb1-5138f70e336d>
2010-04-05 00:16:11 +0000

    Added headers, and minor client work

---
+++ trunk/src/put.h
@@ -0,0 +1,13 @@
+/* 
+ * Used by the server and client to send put requests and handle them.
+ *
+ */
+
+#ifndef _PUT_H
+#define	_PUT_H
+
+    void put_handle();
+    void put_request();
+
+#endif	/* _PUT_H */
+
