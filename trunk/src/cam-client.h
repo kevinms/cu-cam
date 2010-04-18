3d4d82e3535697d785e8d8ef6e02cfed3568e207
Drago3711 <Drago3711@177bddce-22ae-11df-9cb1-5138f70e336d>
2010-04-18 02:41:24 +0000

    lots of client stuff, and get stuff

--- trunk/src/cam-client.h
+++ trunk/src/cam-client.h
@@ -9,23 +9,22 @@
 struct action_t {
 	char function;
         int usesGroup;
-        struct list_t *serverList;
-        struct list_t *sockList;
-        char *username;
+        struct list_t *serverList;// a list of servers
+        struct list_t *sockList;  // a list of sockets
+        struct list_t *username;  // a list of char *
         char *fileName;
         char *saveLoc;
-	char *buf;
 };
 
 void clientCNTCCode();
-struct action_t parseCmdArgs(int argc, char **argv);
-int verifyGroup(char **argv, struct action_t job);
-void openAll(struct action_t job);
-void closeAll(struct action_t job);
+void parseCmdArgs(int argc, char **argv, struct action_t *job);
+int verifyGroup(char **argv, struct action_t *job);
+void openAll(struct action_t *job);
+void closeAll(struct action_t *job);
 
-void getC(struct action_t job);
-void putC(struct action_t job);
-void statC(struct action_t job);
+void getC(struct action_t *job);
+void putC(struct action_t *job);
+void statC(struct action_t *job);
 
 #endif	/* _CAM_CLIENT_H */
 
--- trunk/src/get.h
+++ trunk/src/get.h
@@ -3,10 +3,15 @@
  * Used by the sever and client to send get requests and handle them.
  */
 
+
+
 #ifndef _GET_H
 #define	_GET_H
 
+#include "net.h"
+#include "list.h"
+
 	void get_handle();
-	void get_request();
+	int get_request(struct net_t *n, struct list_t *list, char *fileName, char *saveLoc);
 
 #endif	/* _GET_H */
