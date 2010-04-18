48ac2d7cb6082538b144f8fdd75e68a27093e6db
burnstheman <burnstheman@177bddce-22ae-11df-9cb1-5138f70e336d>
2010-04-18 22:19:20 +0000

    oops

--- trunk/src/stat.h
+++ trunk/src/stat.h
@@ -6,12 +6,16 @@
 #ifndef _STAT_H
 #define	_STAT_H
 
-	#define ST_WHO  0  // Show who is logged on
-	#define ST_PROC 1  // Show process a user has running
-	#define ST_LS   2  // List the contents of a server
+#include "command.h"
+#include "net.h"
+#include "list.h"
 
-	void stat_handle();
-	void stat_request();
+#define ST_WHO  0  // Show who is logged on
+#define ST_PROC 1  // Show process a user has running
+#define ST_LS   2  // List the contents of a server
+
+void stat_handle(int sock, struct command_t *cmd);
+void stat_request(struct net_t *n, struct list_t *userName, char flag);
 
 #endif	/* _STAT_H */
 
