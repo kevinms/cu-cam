b3566131dac7b2f632d983dc0c132a4df588fed7
Drago3711 <Drago3711@177bddce-22ae-11df-9cb1-5138f70e336d>
2010-04-18 22:20:00 +0000

    put work

--- src/put.h
+++ src/put.h
@@ -6,6 +6,10 @@
 #ifndef _PUT_H
 #define	_PUT_H
 
+#include "net.h"
+#include "list.h"
+#include "command.h"
+
     void put_handle(int sock, struct command_t *cmd);
     int put_request(struct net_t *n, struct list_t *list, char *fileName, char *saveLoc);
 
