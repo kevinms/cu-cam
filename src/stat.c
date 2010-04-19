2222f5a1d28c742c0311a0f163c1d4b9cb7c870e
kevin4 <kevin4@g.clemson.edu>
2010-04-19 10:21:11 +0000

    fixing up stat

--- src/stat.c
+++ src/stat.c
@@ -4,114 +4,73 @@
 #include <string.h>
 #include "utils.h"
 
-/*Error types
-STAT_MALF_REQ,
-STAT_NOS_USER, 
-STAT_UNK_STAT, 
-STAT_ERROR
-*/
-
-/*    Assumptions:
-	Server will go to stat_handle if the packet is 
-		detected as non-TCP (UDP)
-	Client will go to stat_request if the packet is 
-		detected as non-TCP (UDP)
-*/
-
-
-
-/*    Global portion
-
-	//#define ST_WHO  0  // Show who is logged on
-	//#define ST_PROC 1  // Show process a user has running
-	//#define ST_LS   2  // List the contents of a server
-
-void packet_parse(){
-	int task = -1;
-	int 
-}
-
-
-*/
-
-
 #define BUFLEN 512
 #define NPACK 10
 
-
-/*
-int main(){
-	stat_handle();
-	return 0;
-}
-*/
-
-//    Client portion
 void stat_request(struct net_t *n, struct list_t *userName, char flag)
 {
-	//create packet
 	char *inBuf;
-    char buf[256];
+	char buf[256];
 	int size = 0;
-    int dataSize = 0;
+	int dataSize = 0;
 
-    buf[0] = CMD_STAT;
-    buf[1] = STAT_OK;
-	printf("flag: %d\n",flag);
+	// Setup packet header
+	buf[0] = CMD_STAT;
+	buf[1] = STAT_OK;
 	buf[2] = flag;
+	dataSize += 3;
 
-    dataSize += 3;
-
+	// Add any extra information to the packet (username, filename, etc)
 	if(flag==ST_PROC||flag==ST_LS){
 		fprintf(stderr,"username: %s\n",(char *)userName->head->item);
 		strncpy(buf + dataSize, (char *)userName->head->item, strlen((char *)userName->head->item));
 		dataSize += (strlen((char *)userName->head->item));
 	}
 
-	//send request packet to server
+	// Send request packet to server
 	net_send_tcp(n->sock, buf, dataSize);
 
+	// Recieve reply from server
 	inBuf = net_recv_tcp(n->sock);
-	fprintf(stderr, "inbuf[0]: %d, inbuf[1]: %d\n", inBuf[0],inBuf[1]);
-	//handle errors
-	if(inBuf[0] != CMD_STAT) {
-        printf("Error: Recieved unexpected packet type\n");
-        printf("Error Code: %d %d\n", inBuf[0],inBuf[1]);
-        exit(1);
-    }
-	
-	if(inBuf[1] == STAT_MALF_REQ) {
-        printf("Error: Malformed request, now quitting\n");
-        exit(1);
-    }else
-    if(inBuf[1] == STAT_NOS_USER) {
-        printf("Error: No such user, now quitting\n");
-        exit(1);
-    }
-    else
-    if(inBuf[1] == STAT_UNK_STAT) {
-        printf("Error: Unexpextes statistic return, now quitting\n");
-        exit(1);
-    }
-    else
-    if(inBuf[1] == STAT_ERROR) {
-        printf("Error: An unknown error has occured, now quitting\n");
-        exit(1);
-    }
 
+	// Make sure it is a CMD_GET type
+	if(inBuf[0] != CMD_STAT) {
+		printf("Error: Recieved unexpected packet type\n");
+		printf("Error Code: %d %d\n", inBuf[0],inBuf[1]);
+		exit(1);
+	}
+
+	// Make sure the client status is STAT_OK
+	if(inBuf[1] == STAT_MALF_REQ) {
+		printf("Error: Malformed request, now quitting\n");
+		exit(1);
+	}else if(inBuf[1] == STAT_NOS_USER) {
+		printf("Error: No such user, now quitting\n");
+		exit(1);
+	}
+	else if(inBuf[1] == STAT_UNK_STAT) {
+		printf("Error: Unexpextes statistic return, now quitting\n");
+		exit(1);
+	}
+	else if(inBuf[1] == STAT_ERROR) {
+		printf("Error: An unknown error has occured, now quitting\n");
+		exit(1);
+	}
+
+	// Store the file size
 	dataSize = *(int *)(inBuf+2);
 	dataSize = ntohl(dataSize);
-	fprintf(stderr,"dataSize: %d\n",dataSize);
 
 	free(inBuf);
-
 	buf[0] = CMD_STAT;
 	buf[1] = STAT_OK;
 	size += 2;
+
 	net_send_tcp(n->sock,buf,size);
 
 	net_recv_fragments_tcp(n->sock, &inBuf, dataSize);
 
+	// Print out the recieved stats
 	printf("================================================================================\n");
 	printf("                        Data Recieved From The Server                           \n");
 	printf("================================================================================\n");
@@ -151,18 +110,6 @@ char *runCommand_getResults(char *command, int sock, struct command_t *cmd)
 
 	tempFile = fopen("STAT_temp.temp", "r");
 	fread(result, 1, count, tempFile);
-	
-	
-	/*
-	while( !feof( tempFile ) && tempFile )
-	{
-	ch = fgetc(tempFile);
-	        if (ch == EOF)
-	            break;
-	        result[count] = 
-	        ++count;
-	}
-	*/
 
 	fclose( tempFile ); 
 
@@ -182,26 +129,7 @@ char *runCommand_getResults(char *command, int sock, struct command_t *cmd)
 	net_send_fragments_tcp(sock, result, count, 400);
 	
 	//printf("The file contains this\n\n%s", result);
-
-	
 	//result = (char *)malloc(strlen(result) + count);
-
-	
-	/*
-	char line[100];
-	FILE *tempFile2;
-	  if((tempFile2 = fopen("STAT_temp.temp", "r")) == NULL) {
-	    printf("Error Opening File.\n");
-	    exit(1);
-	  }
-	  	
-	while( fgets(line, sizeof(line), tempFile2) != NULL ) {
-		
-	    strcat(result,line);
-	  }
-	 
-	fclose(tempFile2);
-	*/
 	
 	//Removing temp file
 	remove("STAT_temp.temp");
@@ -274,7 +202,3 @@ void stat_handle(int sock, struct command_t *cmd)
 	
 	return;
 }
-
-
-
-
